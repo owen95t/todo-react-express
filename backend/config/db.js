565575f417c6ffa48562d644512f60d1b5968a5d
owentangjintanakarn <owen.t95@hotmail.com>
2021-11-03 18:37:21 +0700

    Backend: - Added db file

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI || require('./config/secret').uri
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(uri, {
+            serverSelectionTimeoutMS: 5000
+        }).then(() => {
+            console.log('MongoDB Atlas connected')
+        }).catch(e => {
+            console.log(e)
+        })
+    }catch (e) {
+        console.log('Error with connecting to MongoDB')
+        console.log(e)
+    }
+}
+
+module.exports = connectDB
+
