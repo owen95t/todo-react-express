bc71d13372c0ef056a97e7996bd0015af5883ccb
owentangjintanakarn <owen.t95@hotmail.com>
2021-11-04 22:14:25 +0700

    Started to include middlewares: helmet and rate limiter

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const connectDB = require('./config/db.js')
+const connectDB = require('./config/db.js');
+const helmet = require('helmet');
+const limiter = require('express-rate-limit')
 
 //Connect DB
 connectDB();
+
+//RATE LIMITER
+const rateLimiter = limiter({
+  windowMs: 1000 * 60 * 15, // every 15 minutes
+  max: 200
+})
+
+//MIDDLEWARES
+//Helmet
+app.use(helmet());
+app.set('json spaces', 2)
+app.use(rateLimiter)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
